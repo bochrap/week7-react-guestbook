1e5e8abf2ef24406c3f2e2e63a9081da10415a25
bochrap <131978926+bochrap@users.noreply.github.com>
2024-01-18 10:37:12 +0000

    post endpoint

--- server/server.js
+++ server/server.js
@@ -17,3 +17,27 @@ const db = new pg.Pool({ connectionString: dbConnectionString });
 app.listen(PORT, () => {
   console.log(`Server is running at: http://localhost:${PORT}`);
 });
+
+app.get("/", (request, response) => {
+  response.json("You are in the root folder");
+});
+
+app.get("/posts", async (request, response) => {
+  const result = await db.query(`SELECT * FROM posts`);
+  response.json(result.rows);
+});
+
+app.post("/posts", async (request, response) => {
+  const title = request.body.title;
+  const content = request.body.content;
+  const likes = request.body.likes;
+  const category_id = request.body.category_id;
+
+  const newPost = await db.query(`INSERT INTO posts (title, content, likes, category_id) VALUES ($1, $2, $3, $4) RETURNING *`, [
+    title,
+    content,
+    likes,
+    category_id,
+  ]);
+  response.json(newPost.rows[0]);
+});
