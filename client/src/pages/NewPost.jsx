1465e14ccf040aaf40e6bc222c576b384d2fe4d9
bochrap <131978926+bochrap@users.noreply.github.com>
2024-01-18 13:43:59 +0000

    add form fields

--- client/src/pages/NewPost.jsx
+++ client/src/pages/NewPost.jsx
@@ -1,3 +1,22 @@
 export default function NewPost() {
-  return <div>I am a Nwe Pots From Page</div>;
+  return (
+    <div>
+      <h1>Create a new post</h1>
+      <form>
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" name="title" required />
+        <label htmlFor="content">Add content</label>
+        <textarea name="content" id="content" cols="30" rows="10" required></textarea>
+        <button type="submit">Submit</button>
+        <label htmlFor=""></label>
+        <select name="category" id="category" required>
+          <option value="">--Select category--</option>
+          <option value="1">Technology</option>
+          <option value="2">Travel</option>
+          <option value="3">Food</option>
+          <option value="4">Lifestyle</option>
+        </select>
+      </form>
+    </div>
+  );
 }
