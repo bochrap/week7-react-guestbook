3e471d75d29dd07ec1f5484b3e9b5d81d7f88b44
bochrap <131978926+bochrap@users.noreply.github.com>
2024-01-21 16:43:19 +0000

    add liking functionality

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 
 export default function PostPage() {
   const [singlePost, setSinglePost] = useState({});
+  const [wasLiked, setWasLiked] = useState(parseInt(localStorage.getItem("wasLiked")) || 0);
   let { id } = useParams();
 
   function handleDelete() {
@@ -11,7 +12,7 @@ export default function PostPage() {
 
   useEffect(() => {
     getSinglePost();
-  }, []);
+  }, [wasLiked, localStorage, handleLike]);
 
   async function getSinglePost() {
     const response = await fetch(`http://localhost:8080/posts/post/${id}`);
@@ -19,12 +20,31 @@ export default function PostPage() {
     setSinglePost(data[0]);
   }
 
+  function handleLike() {
+    if (wasLiked === 0 || wasLiked === undefined) {
+      setWasLiked(1);
+      localStorage.setItem("wasLiked", 1);
+      fetch(`http://localhost:8080/posts/post/${id}/like`, { method: `PUT` });
+      console.log("The post was liked");
+    } else {
+      setWasLiked(0);
+      localStorage.setItem("wasLiked", 0);
+      fetch(`http://localhost:8080/posts/post/${id}/unlike`, { method: `PUT` });
+      console.log("The post was unlked");
+    }
+  }
+
   return (
     <div>
       <p>{singlePost.id}</p>
       <p>{singlePost.title}</p>
       <p>{singlePost.content}</p>
-      <p>❤️ {singlePost.likes}</p>
+      <p>
+        ❤️ {singlePost.likes}
+        <span>
+          <button onClick={handleLike}>Like</button>
+        </span>
+      </p>
       <button onClick={handleDelete}>DELETE POST</button>
     </div>
   );
